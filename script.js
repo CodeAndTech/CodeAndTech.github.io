9e5b1aa1cc8e88d3d54afd09682ed3e0d1e3ddfa
Naufal Fadriansyah <95693500f@gmail.com>
2020-03-26 18:51:22 +0700

    'alomost done

--- script.js
+++ script.js
@@ -7,11 +7,67 @@ let yes = [
 let no = [
     'Alhamdulillah anda sehat, Lakukan Social Distancing dan perkuat imun tubuh anda dengan cara berolahraga dan makan makanan yang bergizi',
     'Anda termasuk dalam pasien dengan kondisi RINGAN lakukan isolasi mandiri, jika kondisi semakin buruk segera pergi ke fasilitas kesahatan untuk pemeriksaan lebih lanjut',
-    'Berdasarkan data yang telah anda berikan maka anda termasuk ke dalam pasien dengan kondisi SEDANG, lakukan isolasi mandiri, dan jika kondisi semakin buruk segera pergi ke fasilitas kesahatan untuk pemeriksaan lebih lanjut'
+    'Berdasarkan data yang telah anda berikan maka anda termasuk ke dalam pasien dengan kondisi SEDANG, lakukan isolasi mandiri, dan jika kondisi semakin buruk segera pergi ke fasilitas kesahatan untuk pemeriksaan lebih lanjut', 'Berdasarkan data yang telah anda berikan maka anda termasuk ke dalam pasien dengan kondisi SEDANG, lakukan isolasi mandiri, dan jika muncul tanda-tanda seperti batuk, sesak nafas, demam, dll. segera pergi ke fasilitas kesahatan untuk pemeriksaan lebih lanjut'
 ]
-
+let a = 0
+let b = 0
 const span = document.querySelector('.message span')
 console.log(span)
+
+
+
+function y() {
+    if (a == 0) {
+        span.innerHTML = yes[0]
+        a++
+        console.log(a)
+    } else if (a == 1) {
+        span.innerHTML = yes[1]
+        a++
+        console.log(a)
+    } else if (a == 2) {
+        span.innerHTML = yes[2]
+        a++
+        console.log(a)
+    } else if (a == 3) {
+        span.innerHTML = yes[3]
+        a++
+        console.log(a)
+    } else if (a == 4) {
+        span.innerHTML = yes[4]
+        a++
+        console.log(a)
+    }
+    if (a == 2 && b == 1) {
+        span.innerHTML = no[3]
+
+    }
+    if (a == 4 && b == 1) {
+        span.innerHTML = yes[4]
+    }
+}
+
+function name() {
+    if (a == 0) {
+        span.innerHTML = no[0]
+        console.log(a)
+    } else if (a == 1) {
+        span.innerHTML = yes[3]
+        b++
+        console.log(b)
+    } else if (a == 2) {
+        span.innerHTML = yes[3]
+        b++
+        console.log(b)
+    }
+    if (b == 2) {
+        span.innerHTML = no[1]
+        b++
+        console.log(b)
+    }
+}
+
+
 
 const btnLight = document.querySelectorAll('button')
 btnLight[0].addEventListener('click', function e() {
@@ -22,56 +78,3 @@ btnLight[0].addEventListener('click', function e() {
 btnLight[1].addEventListener('click', function e() {
     name()
 })
-
-
-let a = 0
-
-function y() {
-    if (a == 0) {
-        span.innerHTML = yes[0]
-            ++a
-    } else if (a == 1) {
-        span.innerHTML = yes[1]
-        a += 2
-    } else if (a == 2) {
-        span.innerHTML = yes[2]
-            ++a
-    } else if (a == 3) {
-        span.innerHTML = no[2]
-            ++a
-    } else if (a == 4) {
-        span.innerHTML = yes[4]
-            ++a
-    } else if (a == 5) {
-        span.innerHTML = no[2]
-
-    }
-    console.log(a)
-}
-
-function name() {
-    console.log(a)
-    if (a == 0) {
-        span.innerHTML = no[0]
-        btnLight[0].disabled = true;
-    } else if (a == 1) {
-        span.innerHTML = yes[3]
-        // btnLight[0].disabled = true;
-        a += 2
-    } else if (a == 2) {
-        span.innerHTML = yes[3]
-        // btnLight[0].disabled = true;
-        a++
-    } else if (a == 3) {
-        span.innerHTML = no[1]
-
-    } else if (a == 4) {
-        span.innerHTML = no[2]
-        btnLight[0].disabled = true;
-        btnLight[1].disabled = true;
-    } else if (a == 5) {
-        span.innerHTML = no[1]
-        btnLight[0].disabled = true;
-        btnLight[1].disabled = true;
-    }
-}
