a7771ac3e456773ac740cd32fc5753d483f82089
CodeAndTech <solvo1233@gmail.com>
2020-03-28 11:24:41 +0700

    almost done!

--- script.js
+++ script.js
@@ -26,7 +26,9 @@ function y() {
         a++
         console.log(a)
     } else if (a == 1) {
+
         ul.appendChild(yes_1)
+
         a++
         console.log(a)
     } else if (a == 2) {
@@ -48,22 +50,27 @@ function y() {
         console.log(a)
     }
     if (a == 2 && b == 1) {
-        span.innerHTML = no[3]
+        yes_1.style.display = 'none'
+        no_3.style.backgroundColor = 'rgb(196, 206, 64)'
+        ul.appendChild(no_3)
+        parent_button.appendChild(button_reefresh)
         btnLight[0].disabled = true
         btnLight[1].disabled = true
 
     }
     if (a == 4 && b == 1) {
-        span.innerHTML = no[2]
+
+        ul.appendChild(no_2)
     }
     if (a == 4 && b == 2) {
-        span.innerHTML = no[2]
+        ul.appendChild(no_2)
     }
 }
 
 function name() {
     if (a == 0) {
         ul.appendChild(no_0)
+        parent_button.appendChild(button_reefresh)
         console.log(a)
         btnLight[0].disabled = true
         btnLight[1].disabled = true
@@ -72,20 +79,22 @@ function name() {
         b++
         console.log(b)
     } else if (a == 2) {
-        span.innerHTML = yes[3]
+        ul.appendChild(yes_3)
         b++
         console.log(b)
     } else if (a == 3) {
-        span.innerHTML = yes[3]
+        ul.appendChild(yes_3)
         b++
     } else if (a == 4) {
-        span.innerHTML = no[2]
+        ul.appendChild(no_2)
+        parent_button.appendChild(button_reefresh)
         btnLight[0].disabled = true
         btnLight[1].disabled = true
     }
     if (b == 2) {
         no_1.style.backgroundColor = 'rgb(96, 196, 109)'
         ul.appendChild(no_1)
+        parent_button.appendChild(button_reefresh)
         b++
         btnLight[0].disabled = true
         btnLight[1].disabled = true
@@ -115,7 +124,7 @@ btnLight[1].addEventListener('click', function e() {
 // button.forEach(e => button_yes.setAttribute('class', `${Date.now()}`))
 const parent_button = document.querySelector('.button')
 const button_reefresh = document.createElement('button')
-const isi_button = document.createTextNode('Refresh')
+const isi_button = document.createTextNode('Try again..')
 button_reefresh.appendChild(isi_button)
 button_reefresh.classList.add('btn-primary')
 button_reefresh.addEventListener('click', function (e) {
@@ -123,9 +132,30 @@ button_reefresh.addEventListener('click', function (e) {
 })
 
 const link_rujukan = document.createElement('a')
-const isi_link = document.createTextNode('. Informasi lebih lanjut bisa klik disini')
+const isi_link = document.createTextNode(' . Informasi lebih lanjut bisa klik disini')
 link_rujukan.appendChild(isi_link)
 link_rujukan.setAttribute('href', `https://www.covid19.go.id/`)
+
+const link_rujukan_1 = document.createElement('a')
+const isi_link_1 = document.createTextNode(' . Informasi lebih lanjut bisa klik disini')
+link_rujukan_1.appendChild(isi_link_1)
+link_rujukan_1.setAttribute('href', `https://www.covid19.go.id/`)
+
+const link_rujukan_2 = document.createElement('a')
+const isi_link_2 = document.createTextNode(' . Informasi lebih lanjut bisa klik disini')
+link_rujukan_2.appendChild(isi_link_2)
+link_rujukan_2.setAttribute('href', `https://www.covid19.go.id/`)
+
+const link_rujukan_3 = document.createElement('a')
+const isi_link_3 = document.createTextNode(' . Informasi lebih lanjut bisa klik disini')
+link_rujukan_3.appendChild(isi_link_3)
+link_rujukan_3.setAttribute('href', `https://www.covid19.go.id/`)
+
+const link_rujukan_4 = document.createElement('a')
+const isi_link_4 = document.createTextNode(' . Informasi lebih lanjut bisa klik disini')
+link_rujukan_4.appendChild(isi_link_4)
+link_rujukan_4.setAttribute('href', `https://www.covid19.go.id/`)
+
 
 
 
@@ -155,18 +185,26 @@ yes_3.appendChild(isi_New_Li_3)
 const yes_4 = document.createElement('span')
 const isi_New_Li_4 = document.createTextNode(`${yes[4]}`)
 yes_4.appendChild(isi_New_Li_4)
-yes_4.appendChild(link_rujukan)
+yes_4.appendChild(link_rujukan_1)
 
 
 const no_0 = document.createElement('span')
 const isi_New_no_0 = document.createTextNode(`${no[0]}`)
 no_0.appendChild(isi_New_no_0)
+no_0.appendChild(link_rujukan)
 
 
 const no_1 = document.createElement('span')
 const isi_New_no_1 = document.createTextNode(`${no[1]}`)
 no_1.appendChild(isi_New_no_1)
+no_1.appendChild(link_rujukan_2)
 
 const no_2 = document.createElement('span')
 const isi_New_no_2 = document.createTextNode(`${no[2]}`)
 no_2.appendChild(isi_New_no_2)
+no_2.appendChild(link_rujukan_3)
+
+const no_3 = document.createElement('span')
+const isi_New_no_3 = document.createTextNode(`${no[3]}`)
+no_3.appendChild(isi_New_no_3)
+no_3.appendChild(link_rujukan_4)
